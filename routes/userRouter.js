319b18ea0470ce8ebf8d5ed50aeda56cb561b99c
Alan Perez <alanp9six@gmail.com>
2019-08-05 13:11:37 -0500

    :fire: User Deleted from DB

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 //MODEL
 const Users = require('../data/db')
 
+// GET ALL USERS
 router.get("/", async(req,res) => {
   try {
     const users = await Users.find()
@@ -14,6 +15,27 @@ router.get("/", async(req,res) => {
   }
 })
 
+//GET USER BY ID
 
+router.get("/:id", async(req,res) => {
+  const id = req.params.id;
+  
+})
+
+
+// POST
+
+// DELETE
+router.delete("/:id", (req,res) => {
+  const id = req.params.id;
+
+  Users.remove(id)
+    .then(user => {
+      res.status(200).json({ message: 'user deleted' })
+    }).catch(error => {
+      res.status(500).json({ error: 'Could not delete user by this id'})
+    })
+})
+// UPDATE
 
 module.exports = router;
