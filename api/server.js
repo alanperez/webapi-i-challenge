8c3940dbb8a5d3a82e70e204615b73603e716e68
Alan Perez <alanp9six@gmail.com>
2019-08-05 12:56:40 -0500

    :sparkles: setup middleware, user route, and endpoint

--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const server = express();
-
-// middleware
 const serverConfig = require('./serverConfig')
+// middleware
+serverConfig(server)
 
 // routes
+const userRouter = require('../routes/userRouter')
 
 
+//endpoints 
+server.use("/users", userRouter)
+
+server.get('/', (req,res) => {
+  res.send('The Homies')
+})
 
 module.exports = server;
